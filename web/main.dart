2027e88d8257cd09ad554734155ef8b352fc7a03
Graham MacDonald <grahamamacdonald@gmail.com>
2013-11-15 13:08:01 +0000

    Got a grid!

--- web/main.dart
+++ web/main.dart
@@ -1,17 +1,42 @@
 import 'package:angular/angular.dart';
+import 'dart:math';
 
 @NgController(
-    selector: '[life-grid]',
+    selector: '[life]',
     publishAs: 'ctrl')
 class LifeController {
 
-  List<bool> grid;
-
+  List<List<Cell>> grid;
+  
   LifeController() {
-    grid = [0,0,0,1,0,1,0,0,0];
+    var rnd = new Random();
+    
+    grid = new List<List<Cell>>();
+    
+    var numRows = 10;
+    var numCols = 10;
+    for (var y = 0; y < numRows; y++)
+    {
+      var row = new List<Cell>();
+      grid.add(row);
+      
+      for (var x = 0; x < numCols; x++)
+      {
+        var id = y * numCols + x;
+        row.add(new Cell(id, rnd.nextBool()));
+      }
+    }
   }
-
 }
+
+
+class Cell {
+  int id;
+  bool visible;
+  
+  Cell(this.id, this.visible);
+}
+
 
 class MyAppModule extends Module {
   MyAppModule() {
